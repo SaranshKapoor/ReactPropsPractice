1863d8001226378cdfab487c96eaa8beb7a718b0
SaranshKapoor <31321155+SaranshKapoor@users.noreply.github.com>
2021-10-26 15:58:32 +0530

    Javascript Map Function Integration

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,28 +2,22 @@ import React from "react";
 import Contact from "./Contact";
 import contacts from "../contacts";
 
+function CreateContact(contact) {
+  return (
+    <Contact
+      key={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      phn={contact.phone}
+      email={contact.email}
+    />
+  );
+}
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <Contact
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        phn={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Contact
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        phn={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Contact
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        phn={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      {contacts.map(CreateContact)}
     </div>
   );
 }
